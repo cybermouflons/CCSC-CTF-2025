32fa38729a3dd2a20e69994230443b7efc0666d0
tratrafe2 <tratrafe2@gmail.com>
2025-06-19 19:19:23 +0300

    update: You've got a flag in me Upload files

--- misc/You've got a flag in me/solution/readmidi.py
+++ misc/You've got a flag in me/solution/readmidi.py
@@ -1,6 +1,6 @@
 import pretty_midi
 
-K = [7,58,391,58,129,80,537,80,389,33,80,107,522,391,389,148,386,522,389,58,240,240,107,1]
+K=[7,58,391,58,129,80,537,80,389,33,80,107,522,391,389,148,386,522,389,58,240,240,107,1]
 
 midi_data = pretty_midi.PrettyMIDI("../public/flag.midi")
 instrument = pretty_midi.Instrument(program=0)  
